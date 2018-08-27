c068986c57ad9f65236ca9d54e03915073e6af6b
Nate Cottle <cottle.nate@gmail.com>
2018-08-27 11:36:19 -0700

    Add bootstrap

--- src/app/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export const masterFirebaseConfig = {
-  apiKey: "AIzaSyDHB4dHsHXn9Ri9x90npqzcXBxPPHcqyKs",
-  authDomain: "angular-team-week.firebaseapp.com",
-  databaseURL: "https://angular-team-week.firebaseio.com",
-  projectId: "angular-team-week",
-  storageBucket: "angular-team-week.appspot.com",
-  messagingSenderId: "778474208654"
-}
