a53a895c75c15711106e31ee2e5edb99483098d9
reeseglasscock <reesegpdx@gmail.com>
2018-08-27 15:59:01 -0700

    create new usercomments from models

--- src/app/components/subscribe-form/subscribe-form.component.ts
+++ src/app/components/subscribe-form/subscribe-form.component.ts
@@ -14,5 +14,8 @@ export class SubscribeFormComponent implements OnInit {
   }
 
   submitComment(name:string, email:string, message:string) {
+    let newComment: UserComments = new UserComments(name, email, message)
+    console.log(newComment);
+    
   }
 }
