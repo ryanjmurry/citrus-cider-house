2033444f60f50f851a82a50ea482308d7f75bd27
Ryan Murry <ryanjmurry@gmail.com>
2018-08-28 00:06:22 -0700

    edit cider class properties and constructor

--- src/app/models/cider.ts
+++ src/app/models/cider.ts
@@ -1,10 +1,8 @@
 export class Cider {
-  constructor(   
-    public key: string,
-    public name: string,
-    public image: string,
-    public abv: number,
-    public acidity: number,
-    public brix: number
-  ) {}
+  key: string;
+  name: string;
+  imagePath: string;
+  abv: number;
+  ta: number;
+  brix: number;
 }
