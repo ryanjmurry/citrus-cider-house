80a72c4f726d02b496933d06dc92c4008887cfb5
reeseglasscock <reesegpdx@gmail.com>
2018-08-27 20:43:31 -0700

    update service to use angularfire v5

--- src/app/services/comments.service.ts
+++ src/app/services/comments.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { UserComments } from '../models/user-comments';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CommentsService {
 
-  constructor() { }
+  comments: AngularFireList<any[]>;
+
+  constructor(private afDb: AngularFireDatabase) {
+    afDb.list<UserComments>('/userComments').valueChanges().subscribe(console.log);
+    this.comments = afDb.list('userComments')
+  }
+
+  getComments(){
+    return this.comments;
+  }
 }
