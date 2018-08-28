883eacc27e86ab9e5a9eafa4ba6b89d57fe68762
Ryan Murry <ryanjmurry@gmail.com>
2018-08-27 23:22:59 -0700

    create cider class

---
+++ src/app/models/cider.ts
@@ -0,0 +1,10 @@
+export class Cider {
+  constructor(   
+    public key: string,
+    public name: string,
+    public image: string,
+    public abv: number,
+    public acidity: number,
+    public brix: number
+  ) {}
+}
