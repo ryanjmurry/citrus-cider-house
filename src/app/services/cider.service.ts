cdad8fef650c78be4e3902d34bbcbae622cbb591
Ryan Murry <ryanjmurry@gmail.com>
2018-08-27 23:33:36 -0700

    create crud methods

--- src/app/services/cider.service.ts
+++ src/app/services/cider.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Cider } from '../models/cider';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CiderService {
 
-  constructor() { }
+  private dbPath = '/ciders'
+
+  cidersRef: AngularFireList<Cider> = null;
+
+  constructor(private db: AngularFireDatabase) { 
+    this.cidersRef = db.list(this.dbPath);
+  }
+
+  createCider(cider: Cider): void {
+    this.cidersRef.push(cider);
+  }
+
+  updateCider(key: string, value: any): void {
+    this.cidersRef.update(key, value).catch(error => this.handleError(error));
+  }
+
+  deleteCider(key: string): void {
+    this.cidersRef.remove(key).catch(error => this.handleError(error));
+  }
+
+  getCidersList(): AngularFireList<Cider> {
+    return this.cidersRef;
+  }
+
+  deleteAll(): void {
+    this.cidersRef.remove().catch(error => this.handleError(error));
+  }
+
+  
+
+  private handleError(error) {
+    console.log(error);
+  }
 }
