621c57f99e2dfc70a35967797e3d9fc8db2703d4
Ryan Murry <ryanjmurry@gmail.com>
2018-08-28 10:22:51 -0700

    add logic to delete and update specific menu items

--- src/app/components/menu/menu-details/menu-details.component.ts
+++ src/app/components/menu/menu-details/menu-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MenuItem } from '../../../models/menu-item';
+import { MenuItemService } from '../../../services/menu-item.service';
 
 @Component({
   selector: 'app-menu-details',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() menuItem: MenuItem;
+
+  updateIsActive: boolean = false;
+
+  constructor(private menuItemService: MenuItemService) { }
 
   ngOnInit() {
+  }
+
+  showUpdateForm() {
+    this.updateIsActive = true;
+  }
+
+  deleteMenuItem() {
+    this.menuItemService.deleteMenuItem(this.menuItem.key);
+  }
+
+  onSubmit(key: string, updateName: string, updatePrice: string, updateDescription) {
+    this.updateIsActive = false;
+
+    this.menuItemService.updateMenuItem(key, {
+      name: updateName,
+      price: updatePrice,
+      description: updateDescription
+    });
   }
 
 }
