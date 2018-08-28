026b808548e0f86f64358998d4e29b33eadbdd9d
reeseglasscock <reesegpdx@gmail.com>
2018-08-27 21:14:44 -0700

    add comments service to component

--- src/app/components/subscribe-form/subscribe-form.component.ts
+++ src/app/components/subscribe-form/subscribe-form.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { UserComments } from "../../models/user-comments";
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { CommentsService} from '../../services/comments.service';
 
 @Component({
   selector: 'app-subscribe-form',
   templateUrl: './subscribe-form.component.html',
-  styleUrls: ['./subscribe-form.component.css']
+  styleUrls: ['./subscribe-form.component.css'],
+  providers: [CommentsService]
 })
 export class SubscribeFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private commentsService: CommentsService) {
+  }
 
   ngOnInit() {
   }
@@ -16,6 +21,5 @@ export class SubscribeFormComponent implements OnInit {
   submitComment(name:string, email:string, message:string) {
     let newComment: UserComments = new UserComments(name, email, message)
     console.log(newComment);
-    
   }
 }
