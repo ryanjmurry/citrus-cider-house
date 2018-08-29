9532260af301f1c50d1808a612380963da457a72
Ryan Murry <ryanjmurry@gmail.com>
2018-08-29 15:54:42 -0700

    add form and methods to create new cider

--- src/app/components/admin/admin-ciders/admin-ciders-create/admin-ciders-create.component.ts
+++ src/app/components/admin/admin-ciders/admin-ciders-create/admin-ciders-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Cider } from '../../../../models/cider';
+import { map } from 'rxjs/operators';
+import { CiderService } from '../../../../services/cider.service';
 
 @Component({
   selector: 'app-admin-ciders-create',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminCidersCreateComponent implements OnInit {
 
-  constructor() { }
+  cider: Cider = new Cider();
+  ciders: any;
+  submitted = false;
+
+  constructor(private ciderService: CiderService) { }
 
   ngOnInit() {
+    this.getCidersList();
   }
 
+  getCidersList() {
+    this.ciderService.getCidersList().snapshotChanges().pipe(
+      map(changes => 
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() }))
+      )
+    ).subscribe(ciders => {
+      this.ciders = ciders;
+    });
+  }
+
+  newCider(): void {
+    this.submitted = false;
+    this.cider = new Cider();
+  }
+
+  save() {
+    this.ciderService.createCider(this.cider);
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
 }
