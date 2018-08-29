329feae919e887f62c4c9eeb2a0b2a0717bd6b34
reeseglasscock <reesegpdx@gmail.com>
2018-08-29 11:37:46 -0700

    add ngbootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -30,6 +31,7 @@ import { AdminComponent } from './components/admin/admin.component';
 import { PublishCommentComponent } from './components/admin/publish-comment/publish-comment.component';
 import { AuthService } from './services/auth.service';
 import { AdminGuard } from './guards/admin.guard';
+import { CommentsCarouselComponent } from './components/comments-carousel/comments-carousel.component';
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -58,6 +60,7 @@ export const firebaseConfig = {
     MenuCreateComponent,
     AdminComponent,
     PublishCommentComponent,
+    CommentsCarouselComponent,
   ],
   imports: [
     BrowserModule,
@@ -69,7 +72,8 @@ export const firebaseConfig = {
     AngularFireModule.initializeApp(firebaseConfig),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    NgbModule
   ],
   providers: [AuthService, AdminGuard],
   bootstrap: [AppComponent]
