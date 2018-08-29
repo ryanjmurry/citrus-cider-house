e421fbf92f056c434343f8896dede808b3575131
reeseglasscock <reesegpdx@gmail.com>
2018-08-29 13:52:06 -0700

    adjust carousel settings to remove navigation arrows

--- src/app/components/comments-carousel/comments-carousel.component.ts
+++ src/app/components/comments-carousel/comments-carousel.component.ts
@@ -10,14 +10,13 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class CommentsCarouselComponent implements OnInit {
 
-  images = [1, 2, 3, 4].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
-
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
-    config.interval = 10000;
-    config.wrap = false;
+    config.interval = 5000;
+    config.wrap = true;
     config.keyboard = false;
     config.pauseOnHover = false;
+    config.showNavigationArrows = false;
   }
 
   ngOnInit() {
