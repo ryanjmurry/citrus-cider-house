814089ff44bd40ad6ed42315767cbc7c6d6ee03c
Ryan Murry <ryanjmurry@gmail.com>
2018-08-29 11:34:20 -0700

    add imports and methods to perform CRUD functions on users in db

--- src/app/services/user-data.service.ts
+++ src/app/services/user-data.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from 'angularfire2/firestore';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserDataService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) {
+  }
+
+  getAllUsers() {
+    return this.afs.collection('users').snapshotChanges();
+  }
+
+  getUser(uid: string) {
+    return this.afs.collection('users').doc(uid).snapshotChanges();
+  }
+
+  giveAdminRole(uid: string) {
+    return this.afs.collection('users').doc(uid).update({ roles: { admin: true, customer: true } });
+  }
+
+  removeAdminRole(uid: string) {
+    return this.afs.collection('users').doc(uid).update({ roles: { admin: false, customer: true } });
+  }
+
+
 }
