d53f63aa41b8cb4981329707eeade566fe4e2348
Ryan Murry <ryanjmurry@gmail.com>
2018-08-29 15:55:34 -0700

    add ciders details list and methods to update and delete ciders

--- src/app/components/admin/admin-ciders/admin-ciders-details/admin-ciders-details.component.ts
+++ src/app/components/admin/admin-ciders/admin-ciders-details/admin-ciders-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Cider } from '../../../../models/cider';
+import { CiderService } from '../../../../services/cider.service';
 
 @Component({
   selector: 'app-admin-ciders-details',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminCidersDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() cider: Cider;
+
+  updateIsActive: boolean = false;
+
+  constructor(private ciderService: CiderService) { }
 
   ngOnInit() {
   }
 
+  showUpdateForm() {
+    this.updateIsActive = true;
+  }
+
+  deleteCider() {
+    this.ciderService.deleteCider(this.cider.key);
+  }
+
+  onSubmit(key:string, updateName: string, updateImage: string, updateABV: string, updateTA: string, updateBrix: string) {
+    this.updateIsActive = false;
+
+    this.ciderService.updateCider(key, {
+      name: updateName,
+      image: updateImage,
+      abv: updateABV,
+      ta: updateTA,
+      brix: updateBrix
+    });
+  }
 }
