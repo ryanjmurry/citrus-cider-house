cbcd49071f1bb720b4d47b82699056fb81f72561
Ryan Murry <ryanjmurry@gmail.com>
2018-08-30 09:18:03 -0700

    add methods to save new merch item to db

--- src/app/components/admin/admin-merch/admin-merch-create/admin-merch-create.component.ts
+++ src/app/components/admin/admin-merch/admin-merch-create/admin-merch-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MerchItem } from '../../../../models/merch-item';
+import { MerchItemService } from '../../../../services/merch-item.service';
+
 
 @Component({
   selector: 'app-admin-merch-create',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminMerchCreateComponent implements OnInit {
 
-  constructor() { }
+  merchItem: MerchItem = new MerchItem();
+  submitted = false;
+
+  constructor(private merchItemService: MerchItemService) { }
 
   ngOnInit() {
   }
 
+  newMerchItem(): void {
+    this.submitted = false;
+    this.merchItem = new MerchItem();
+  }
+
+  save() {
+    this.merchItemService.createMerchItem(this.merchItem);
+    this.merchItem = new MerchItem();
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
 }
