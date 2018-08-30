0190c88d471cffa903bb5e1e8ead0b7e6ea3e67f
Ryan Murry <ryanjmurry@gmail.com>
2018-08-30 09:18:25 -0700

    add logic to delete and update merch items in db

--- src/app/components/admin/admin-merch/admin-merch-details/admin-merch-details.component.ts
+++ src/app/components/admin/admin-merch/admin-merch-details/admin-merch-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MerchItem } from '../../../../models/merch-item';
+import { MerchItemService } from '../../../../services/merch-item.service';
 
 @Component({
   selector: 'app-admin-merch-details',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminMerchDetailsComponent implements OnInit {
 
-  constructor() { }
+  @Input() merchItem: MerchItem;
+
+  updateIsActive: boolean = false;
+
+  constructor(private merchItemService: MerchItemService) { }
 
   ngOnInit() {
   }
 
+  showUpdateForm() {
+    this.updateIsActive = true;
+  }
+
+  deleteMerchItem() {
+    this.merchItemService.deleteMerchItem(this.merchItem.key);
+  }
+
+  onSubmit(key: string, updateName: string, updatePrice: string, updateDescription) {
+    this.updateIsActive = false;
+
+    this.merchItemService.updateMerchItem(key, {
+      name: updateName,
+      price: updatePrice,
+      description: updateDescription
+    });
+  }
 }
