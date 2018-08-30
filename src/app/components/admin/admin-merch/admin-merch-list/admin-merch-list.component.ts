58ec74f20b1be7dd1e651377f44f12b3b571ff8b
Ryan Murry <ryanjmurry@gmail.com>
2018-08-30 09:19:06 -0700

    add methods to retrieve all merch items and delete all merch items from db

--- src/app/components/admin/admin-merch/admin-merch-list/admin-merch-list.component.ts
+++ src/app/components/admin/admin-merch/admin-merch-list/admin-merch-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { MerchItemService } from '../../../../services/merch-item.service';
 
 @Component({
   selector: 'app-admin-merch-list',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-merch-list.component.css']
 })
 export class AdminMerchListComponent implements OnInit {
+ 
+  merchItems: any;
 
-  constructor() { }
+  constructor(private merchItemService: MerchItemService) { }
 
   ngOnInit() {
+    this.getMerchItemsList();
   }
 
+  getMerchItemsList() {
+    this.merchItemService.getMerchItemsList().snapshotChanges().pipe(
+      map(changes =>
+        changes.map(c => ({ key: c.payload.key, ...c.payload.val() })) 
+      )
+    ).subscribe(merchItems => {
+      this.merchItems = merchItems;
+    });
+  }
+
+  deleteMerchItems() {
+    this.merchItemService.deleteAll();
+  }
 }
